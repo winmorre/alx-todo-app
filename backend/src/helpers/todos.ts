61766e84360a8d616a371c5bd07f21afbea7caaa
reeko234 <jasonjoywinmorre@gmail.com>
2022-10-09 04:21:32 +0000

    Fixed comments from review

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -7,17 +7,17 @@ import { createLogger } from '../utils/logger'
 import * as createError from 'http-errors'
 
 
-const logger = createLogger("todos")
+const logger = createLogger('todos')
 
 const attachmentUtils = new AttachmentUtils()
 const todosAccess = new TodosAccess()
 
 
 export const getTodosForUser = async (userId: string): Promise<TodoItem[]> => {
-  try{
+  try {
     return await todosAccess.getTodosForUser(userId)
-  }catch (e) {
-    logger.error("Get User Todos Error",{
+  } catch (e) {
+    logger.error('Get User Todos Error', {
       userId: userId,
       error: e.message
     })
@@ -26,24 +26,24 @@ export const getTodosForUser = async (userId: string): Promise<TodoItem[]> => {
 }
 
 export const createTodo = async (payload: CreateTodoRequest, userId: string): Promise<TodoItem> => {
-  try{
+  try {
     return await todosAccess.createTodo(payload, userId)
-  }catch (e) {
-    logger.error('Todo Create Error',{
+  } catch (e) {
+    logger.error('Todo Create Error', {
       error: e.message
     })
   }
 }
 
-export const updateTodo = async (payload: UpdateTodoRequest, todoId: string,userId:string): Promise<number> => {
-  logger.info('Update User todo',{
+export const updateTodo = async (payload: UpdateTodoRequest, todoId: string, userId: string): Promise<number> => {
+  logger.info('Update User todo', {
     todoId: todoId
   })
   try {
-    return await todosAccess.updateTodo(payload,userId,todoId)
-  }catch (e) {
-    logger.error('Todo Update Error',{
-      todoId:todoId,
+    return await todosAccess.updateTodo(payload, userId, todoId)
+  } catch (e) {
+    logger.error('Todo Update Error', {
+      todoId: todoId,
       payload: JSON.stringify(payload),
       error: e.message
     })
@@ -52,20 +52,35 @@ export const updateTodo = async (payload: UpdateTodoRequest, todoId: string,user
 }
 
 
-export const deleteTodo = async (todoId: string,userId:string)=>{
-  try{
+export const deleteTodo = async (todoId: string,userId:string) => {
+  try {
     return await todosAccess.deleteTodo(todoId,userId)
-  }catch (e) {
-    logger.error('Todo Delete Error',{
+  } catch (e) {
+    logger.error('Todo Delete Error', {
       todoId: todoId,
       error: e.message
     })
   }
 }
 
-export const createAttachmentPresignedUrl = async  (todoId:string)=>{
-  logger.info("Create Attachment Pre-signed URL",{
-    todoId:todoId
+export const createAttachmentPresignedUrl = async (todoId: string) => {
+  logger.info('Create Attachment Pre-signed URL', {
+    todoId: todoId
   })
   return await attachmentUtils.createAttachmentPresignedUrl(todoId)
+}
+
+export const imageUrl = async (todoId: string) => {
+  const Aws = require('aws-sdk')
+  const s3 = Aws.S3()
+  const bucketName = process.env.ATTACHMENT_S3_BUCKET
+  const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+  return await s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: parseInt(urlExpiration),
+    ContentType: "image/*",
+    ACL: 'public-write'
+  })
 }
--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -8,40 +8,39 @@ import { createTodo, getTodosForUser } from '../businessLogic/todos'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 
 
-
 const XAWS = AWSXRay.captureAWS(AWS)
 
 const logger = createLogger('TodosAccess')
 
- function createDynamoDbClient() {
-  if (process.env.IS_OFFLINE){
+function createDynamoDbClient() {
+  if (process.env.IS_OFFLINE) {
     logger.info('Creating a local dynamoDb instance')
     // @ts-ignore
-    return  new XAWS.DynamoDB.DocumentClient({
-      region:"localhost",
-      endpoint:"https://localhost:8000"
+    return new XAWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'https://localhost:8000'
     })
 
   }
   // @ts-ignore
-   return  new XAWS.DynamoDB.DocumentClient()
+  return new XAWS.DynamoDB.DocumentClient()
 }
 
 
-export class TodosAccess{
-  constructor(private  readonly docClient:DocumentClient=createDynamoDbClient(),
+export class TodosAccess {
+  constructor(private readonly docClient: DocumentClient = createDynamoDbClient(),
               private readonly todosTable = process.env.TODOS_TABLE) {
   }
 
-  async getTodosForUser(userId:string):Promise<TodoItem[]>{
+  async getTodosForUser(userId: string): Promise<TodoItem[]> {
     return await getTodosForUser(userId)
   }
 
-  async createTodo(payload: CreateTodoRequest, userId: string):Promise<TodoItem>{
+  async createTodo(payload: CreateTodoRequest, userId: string): Promise<TodoItem> {
     return await createTodo(payload, userId)
   }
 
-  async updateTodo(payload:TodoUpdate,userId:string,todoId:string):Promise<number>{
+  async updateTodo(payload: TodoUpdate, userId: string, todoId: string): Promise<number> {
     await this.docClient.update(
       {
         TableName: this.todosTable,
@@ -53,7 +52,7 @@ export class TodosAccess{
           ':name': payload.name,
           ':done': payload.done,
           ':dueDate': payload.dueDate,
-          ':userId':userId
+          ':userId': userId
         },
         ConditionExpression: `userId = :userId`
       }
@@ -62,17 +61,17 @@ export class TodosAccess{
     return 200
   }
 
-  async deleteTodo(todoId: string,userId:string):Promise<number>{
-    await this.docClient.delete({
+  async deleteTodo(todoId: string, userId: string): Promise<string> {
+    const dc = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
+    const item = {
       TableName: this.todosTable,
-      Key: {
-        todoId: todoId
-      },
-      ExpressionAttributeValues: {
-        ':userId':userId
-      },
-      ConditionExpression: `userId = :userId`
-    }).promise()
-    return 200
+      Key:
+        {
+          todoId: todoId.toString(),
+          userId: userId
+        }
+    }
+    await dc.delete(item).promise()
+    return todoId
   }
 }
